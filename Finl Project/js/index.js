614f7bcafd96483e9f5d4f48230bc98e533c8b41
Varanshu <37748099+Varanshu@users.noreply.github.com>
2020-09-21 20:11:13 +0530

    third commit

--- Finl Project/js/index.js
+++ Finl Project/js/index.js
@@ -1,15 +1,4 @@
-// $( document ).ready(function () {
-//   $(".moreToggle").slice(0, 3).show();
-//
-//     $("#loadMore").on('click', function (e) {
-//       e.preventDefault();
-//       $(".moreToggle:hidden").slice(0).slideDown();
-//       if ($(".moreToggle:hidden").length == 0) {
-//         $("#loadMore").fadeOut('slow');
-//         $("#loadLess").fadeIn('slow');
-//       }
-//     });
-//   });
+// Certificate Card More or Less
 $(document).ready(function() {
   var bool = $(".moreToggle").is(":hidden");
   $(".moreToggle").slice(0, 3).show();
@@ -30,7 +19,7 @@ $(document).ready(function() {
   });
 });
 
-
+// Projects Card More or Less
 $(document).ready(function() {
   var bool = $(".projectToggle").is(":hidden");
   $(".projectToggle").slice(0, 2).show();
